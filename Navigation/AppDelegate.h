77f1e229f3e8bfbfe967f1b974acaf82336c5851
hamdy <dimo.hamdy@gmail.com>
2015-09-13 12:40:38 +0200

    Initial Commit

---
+++ Navigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Navigation
+//
+//  Created by Hamdy on 9/13/15.
+//  Copyright (c) 2015 Hamdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
